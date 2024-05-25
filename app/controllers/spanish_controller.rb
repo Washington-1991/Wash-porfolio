581db4e0049f64f21615938dae65fa5829f0e517
Washington Arambillete <milanghdarkoh@hotmail.com>
2024-05-25 22:41:24 +0200

    wip

---
+++ app/controllers/spanish_controller.rb
@@ -0,0 +1,2 @@
+class SpanishController < ApplicationController
+end
---
+++ test/controllers/spanish_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class SpanishControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
