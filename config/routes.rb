e27698db27e4d3867ec606512248ba103fcee1ab
Washington Arambillete <milanghdarkoh@hotmail.com>
2024-10-17 21:37:41 +0200

    wip

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,8 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-  get '/home', to: 'pages#home'  get "aboutme" => "pages#aboutme"
+  get "home" => "pages#home"
+  get "aboutme" => "pages#aboutme"
   get "projects" => "pages#projects"
   get "contact" => "pages#contact"
   get "blank" => "pages#blank"
