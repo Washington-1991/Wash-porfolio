f30b0cd8edf2c1cf0d6fb2b4f2de4141838bd080
Washington Arambillete <milanghdarkoh@hotmail.com>
2024-10-26 22:30:14 +0200

    wip

--- config/routes.rb
+++ config/routes.rb
@@ -13,21 +13,6 @@ Rails.application.routes.draw do
   get "contact" => "pages#contact"
   get "blank" => "pages#blank"
 
-
-  get "home_esp" => "spanish#home_esp"
-  get "aboutme_esp" => "spanish#aboutme_esp"
-  get "projects_esp" => "spanish#projects_esp"
-  get "contact_esp" => "spanish#contact_esp"
-
-  get "home_por" => "portuguese#home_por"
-  get "aboutme_por" => "portuguese#aboutme_por"
-  get "projects_por" => "portuguese#projects_por"
-  get "contact_por" => "portuguese#contact_por"
-
-  get "home_fra" => "french#home_fra"
-  get "aboutme_fra" => "french#aboutme_fra"
-  get "projects_fra" => "french#projects_fra"
-  get "contact_fra" => "french#contact_fra"
   # Defines the root path route ("/")
   # root "posts#index"
 end
