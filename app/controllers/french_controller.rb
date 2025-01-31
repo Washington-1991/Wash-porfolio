c4577c8cc91bc14cc250738b89965c1ec44b8a5a
Washington Arambillete <milanghdarkoh@hotmail.com>
2025-01-31 18:38:06 +0100

    wip

--- app/controllers/french_controller.rb
+++
@@ -1,5 +0,0 @@
-class FrenchController < ApplicationController
-  def home_fra
-    # You can add any necessary logic here
-  end
-end
--- app/controllers/spanish_controller.rb
+++
@@ -1,6 +0,0 @@
-# app/controllers/spanish_controller.rb
-class SpanishController < ApplicationController
-  def home_esp
-    # You can add any necessary logic here
-  end
-end
