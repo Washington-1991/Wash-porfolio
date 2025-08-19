7df0c6cf2c27dc39d8d6948411d4b9f31a0362ae
Washington Arambillete <milanghdarkoh@hotmail.com>
2025-08-19 12:43:22 +0200

    wip

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -13,56 +13,28 @@ SimpleForm.setup do |config|
   # wrapper, change the order or even add your own to the
   # stack. The options given below are used to wrap the
   # whole input.
-  config.wrappers :default, class: :input,
-    hint_class: :field_with_hint, error_class: :field_with_errors, valid_class: :field_without_errors do |b|
-    ## Extensions enabled by default
-    # Any of these extensions can be disabled for a
-    # given input by passing: `f.input EXTENSION_NAME => false`.
-    # You can make any of these extensions optional by
-    # renaming `b.use` to `b.optional`.
-
-    # Determines whether to use HTML5 (:email, :url, ...)
-    # and required attributes
-    b.use :html5
-
-    # Calculates placeholders automatically from I18n
-    # You can also pass a string as f.input placeholder: "Placeholder"
-    b.use :placeholder
-
-    ## Optional extensions
-    # They are disabled unless you pass `f.input EXTENSION_NAME => true`
-    # to the input. If so, they will retrieve the values from the model
-    # if any exists. If you want to enable any of those
-    # extensions by default, you can change `b.optional` to `b.use`.
-
-    # Calculates maxlength from length validations for string inputs
-    # and/or database column lengths
-    b.optional :maxlength
-
-    # Calculate minlength from length validations for string inputs
-    b.optional :minlength
-
-    # Calculates pattern from format validations for string inputs
-    b.optional :pattern
-
-    # Calculates min and max from length validations for numeric inputs
-    b.optional :min_max
-
-    # Calculates readonly automatically from readonly attributes
-    b.optional :readonly
-
-    ## Inputs
-    # b.use :input, class: 'input', error_class: 'is-invalid', valid_class: 'is-valid'
-    b.use :label_input
-    b.use :hint,  wrap_with: { tag: :span, class: :hint }
-    b.use :error, wrap_with: { tag: :span, class: :error }
-
-    ## full_messages_for
-    # If you want to display the full error message for the attribute, you can
-    # use the component :full_error, like:
-    #
-    # b.use :full_error, wrap_with: { tag: :span, class: :error }
-  end
+  # Configuración de wrappers desactivada aquí para evitar duplicidad con simple_form_bootstrap.rb
+  # Si usas Bootstrap, mantén la configuración en simple_form_bootstrap.rb
+  # config.wrappers :default, class: :input,
+  #   hint_class: :field_with_hint, error_class: :field_with_errors, valid_class: :field_without_errors do |b|
+  #   ## Extensions enabled by default
+  #   # Any of these extensions can be disabled for a
+  #   # given input by passing: `f.input EXTENSION_NAME => false`.
+  #   # You can make any of these extensions optional by
+  #   # renaming `b.use` to `b.optional`.
+  #   # b.use :html5
+  #   # b.use :placeholder
+  #   # b.optional :maxlength
+  #   # b.optional :minlength
+  #   # b.optional :pattern
+  #   # b.optional :min_max
+  #   # b.optional :readonly
+  #   # b.use :label_input
+  #   # b.use :input
+  #   # b.use :hint,  wrap_with: { tag: :span, class: :hint }
+  #   # b.use :error, wrap_with: { tag: :span, class: :error }
+  #   # b.use :full_error, wrap_with: { tag: :span, class: :error }
+  # end
 
   # The default wrapper to be used by the FormBuilder.
   config.default_wrapper = :default
