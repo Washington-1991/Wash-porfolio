d8f05bd81407269836cb7454003564efecb78b77
Washington Arambillete <milanghdarkoh@hotmail.com>
2025-08-20 21:46:55 +0200

    wip

--- db/migrate/20250820192659_add_fields_to_users.rb
+++ db/migrate/20250820192659_add_fields_to_users.rb
@@ -1,4 +1,4 @@
-class AddFieldsToUsers < ActiveRecord::Migration[7.2]
+class AddFieldsToUsers < ActiveRecord::Migration[7.1]
   def change
     add_column :users, :name, :string
     add_column :users, :role, :string, default: "client"
