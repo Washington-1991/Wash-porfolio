8ea7d65c83f16aa6fb7af792f95531b4dbfe244b
Washington Arambillete <milanghdarkoh@hotmail.com>
2026-01-15 15:13:48 +0100

    update

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,16 +3,6 @@ class PagesController < ApplicationController
   def home
   end
 
-  def home_layout
-    kind = params[:kind].to_s
-    allowed = %w[desktop laptop tablet mobile]
-    kind = "desktop" unless allowed.include?(kind)
-
-    render "home/layout", locals: { kind: kind }, layout: false
-  end
-
-
-
   def home_esp
   end
 
